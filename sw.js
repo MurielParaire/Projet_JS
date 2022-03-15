3642c1bd7a33d429b03be11522c455f0eef91099
antoskins <75854261+antonypinel@users.noreply.github.com>
2022-03-15 14:20:08 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open('PWA').then((cache) => cache.addAll([
-            "/Projet_JS/index.html",
-            "/Projet_JS/assets/img/favicon.ico",
-            "/Projet_JS/assets/img/pwa-icon.png",  
-            "/Projet_JS/assets/css/style.css",
-            "/Projet_JS/assets/css/style_light.css",
-            "/Projet_JS/assets/fonts/Inter-Bold.ttf",
-            "/Projet_JS/assets/js/main.js",
-            "/Projet_JS/assets/js/theme.js",
-            "/Projet_JS/assets/js/section_switcher.js"
+            "/index.html",
+            "/assets/img/favicon.ico",
+            "/assets/img/pwa-icon.png",  
+            "/assets/css/style.css",
+            "/assets/css/style_light.css",
+            "/assets/fonts/Inter-Bold.ttf",
+            "/assets/js/main.js",
+            "/assets/js/theme.js",
+            "/assets/js/section_switcher.js"
         ])
     )
 )
