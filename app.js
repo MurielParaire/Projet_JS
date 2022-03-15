8a815823c3c95b617fe004e1eba9f4e8c98437c0
antoskins <75854261+antonypinel@users.noreply.github.com>
2022-03-15 14:59:45 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+if ('serviceWorker' in navigator) {
+    
+    navigator.serviceWorker.register('sw.js').then(
+        (registration) => {
+            console.log('Service Worker registration succeeded:', registration)
+        },
+        /*catch*/ (error) => {
+            console.log('Service Worker registration failed:', error)
+        }
+    )
+} 
+
+else {
+    console.log('Service workers are not supported.')
+}
--- sw.js
+++ sw.js
@@ -1,25 +1,26 @@
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open('PWA').then((cache) => cache.addAll([
-            "/Projet_JS/index.html",
-            "/Projet_JS/assets/img/favicon.ico",
-            "/Projet_JS/assets/img/pwa-icon.png",  
-            "/Projet_JS/assets/css/style.css",
-            "/Projet_JS/assets/css/style_light.css",
-            "/Projet_JS/assets/fonts/Inter-Bold.ttf",
-            "/Projet_JS/assets/js/main.js",
-            "/Projet_JS/assets/js/theme.js",
-            "/Projet_JS/assets/js/section_switcher.js"
-        ])
-    )
-)
-})
-
-self.addEventListener('fetch', (e) => {
-    console.log(e);
-    e.respondWith(
-  
-      caches.match(e.request).then((response) => response || fetch(e.request)),
-  
-    );
-});
+            self.addEventListener('install', (e) => {
+              e.waitUntil(
+                  caches.open('my-custom-pwa').then((cache) => cache.addAll([
+                      "/",
+                      "/index.html",
+                      "/assets/img/favicon.ico",
+                      "/assets/img/pwa-icon.png",  
+                      "/assets/css/style.css",
+                      "/assets/css/style_light.css",
+                      "/assets/fonts/Inter-Bold.ttf",
+                      "/assets/js/main.js",
+                      "/assets/js/theme.js",
+                      "/assets/js/section_switcher.js"
+                  ])
+              )
+          )
+          })
+          
+          self.addEventListener('fetch', (e) => {
+              console.log(e);
+              e.respondWith(
+            
+                caches.match(e.request).then((response) => response || fetch(e.request)),
+            
+              );
+          });
