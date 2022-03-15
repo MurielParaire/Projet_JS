217752fcbeeea2c80df096e15029a1a7281db4d9
antoskins <75854261+antonypinel@users.noreply.github.com>
2022-03-15 22:34:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-    .then(() => { console.log("Service Worker Registered"); });
-}
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js');
+};
