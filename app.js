1578e454e8f7ec325688cda15bfafed43489dad8
antoskins <75854261+antonypinel@users.noreply.github.com>
2022-03-15 19:41:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 if ('serviceWorker' in navigator) {
-  // declaring scope manually
-  navigator.serviceWorker.register('/sw.js', { scope: './' }).then(
+  // Register a service worker hosted at the root of the
+  // site using the default scope.
+  navigator.serviceWorker.register('/sw.js').then(
     (registration) => {
       console.log('Service worker registration succeeded:', registration)
     },
