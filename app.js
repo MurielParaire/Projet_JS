799952f8542574b211def4021afd56920329073b
antoskins <75854261+antonypinel@users.noreply.github.com>
2022-03-15 22:30:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/Projet_JS/sw.js').then(function(reg) {
-    // registration worked
-    console.log('Registration succeeded. Scope is ' + reg.scope);
-  }).catch(function(error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
-  });
-};
+    navigator.serviceWorker.register('/sw.js')
+    .then(() => { console.log("Service Worker Registered"); });
+}
