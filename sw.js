979c6e84e87a59c3d64d47dd0785e7fb289e8933
antoskins <75854261+antonypinel@users.noreply.github.com>
2022-03-15 14:21:21 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open('PWA').then((cache) => cache.addAll([
+            "/",
             "/index.html",
             "/assets/img/favicon.ico",
             "/assets/img/pwa-icon.png",  
