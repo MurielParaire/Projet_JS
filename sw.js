cdaec981a7cabe5af78c4bc64a1cac5f890a5c6f
antoskins <75854261+antonypinel@users.noreply.github.com>
2022-03-15 19:34:13 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,42 @@
 self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open('my-custom-pwa').then((cache) => cache.addAll([
-            "/index.html",
-            "/manifest.webmanifest",
-            "/assets/img/favicon.ico",
-            "/assets/img/pwa-icon.png",  
-            "/assets/css/style.css",
-            "/assets/css/style_light.css",
-            "/assets/fonts/Inter-Bold.ttf",
-            "/assets/js/main.js",
-            "/assets/js/theme.js",
-            "/assets/js/section_switcher.js"
-        ])
-    )
-)
-})
+  console.log('[Service Worker] Install');
+});
+
+const cacheName = 'Projet_JS-v1';
+const appShellFiles = [
+    "./index.html"
+    "./manifest.webmanifest",
+    "./assets/img/favicon.ico",
+    "./assets/img/pwa-icon.png",  
+    "./assets/css/style.css",
+    "./assets/css/style_light.css",
+    "./assets/fonts/Inter-Bold.ttf",
+    "./assets/js/main.js",
+    "./assets/js/theme.js",
+    "./assets/js/section_switcher.js"
+];
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(appShellFiles);
+  })());
+});
 
 self.addEventListener('fetch', (e) => {
-    console.log(e);
-    e.respondWith(
-  
-      caches.match(e.request).then((response) => response || fetch(e.request)),
-  
-    );
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) { return r; }
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
 });
-                   
+
+
+
