ac5ef0c0d2101a4e12bfb8641a02c23dcf323ec0
antoskins <75854261+antonypinel@users.noreply.github.com>
2022-03-15 20:27:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,3 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js')
     .then(() => { console.log("Service Worker Registered"); });
 }
-    /*catch*/ (error) => {
-      console.log('Service worker registration failed:', error)
-    }
-  )
-} else {
-  console.log('Service workers are not supported.')
-}
