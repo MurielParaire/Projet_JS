b959d4c63374916022a1df92435a4a9ee3df8e1e
antoskins <75854261+antonypinel@users.noreply.github.com>
2022-03-15 15:18:13 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,27 @@
-            self.addEventListener('install', (e) => {
-              e.waitUntil(
-                  caches.open('PWA').then((cache) => cache.addAll([
-                      "/",
-                      "/manifest.webmanifest",
-                      "/assets/img/favicon.ico",
-                      "/assets/img/pwa-icon.png",  
-                      "/assets/css/style.css",
-                      "/assets/css/style_light.css",
-                      "/assets/fonts/Inter-Bold.ttf",
-                      "/assets/js/main.js",
-                      "/assets/js/theme.js",
-                      "/assets/js/section_switcher.js"
-                  ])
-              )
-          )
-          })
-          
-          self.addEventListener('fetch', (e) => {
-              console.log(e);
-              e.respondWith(
-            
-                caches.match(e.request).then((response) => response || fetch(e.request)),
-            
-              );
-          });
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open('my-custom-pwa').then((cache) => cache.addAll([
+            "/",
+            "/manifest.webmanifest",
+            "/assets/img/favicon.ico",
+            "/assets/img/pwa-icon.png",  
+            "/assets/css/style.css",
+            "/assets/css/style_light.css",
+            "/assets/fonts/Inter-Bold.ttf",
+            "/assets/js/main.js",
+            "/assets/js/theme.js",
+            "/assets/js/section_switcher.js"
+        ])
+    )
+)
+})
+
+self.addEventListener('fetch', (e) => {
+    console.log(e);
+    e.respondWith(
+  
+      caches.match(e.request).then((response) => response || fetch(e.request)),
+  
+    );
+});
+                   
