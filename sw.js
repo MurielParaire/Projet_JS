f5111989ccace31d7c7be3b9500dbfdb9d46f069
antoskins <75854261+antonypinel@users.noreply.github.com>
2022-03-15 12:42:50 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+var cacheName;
+var appShellFiles = [
+  '/Projet_JS/index.html',
+  '/Projet_JS/assets/img/favicon.ico',  
+  '/Projet_JS/assets/css/style.css',
+  '/Projet_JS/assets/css/style_light.css',
+  '/Projet_JS/assets/fonts/Inter-Bold.ttf',
+  '/Projet_JS/app.js',
+  '/Projet_JS/assets/js/main.js',
+  '/Projet_JS/assets/js/theme.js',
+  '/Projet_JS/assets/js/section_switcher.js'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(appShellFiles);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then(function(response) {
+        return caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
