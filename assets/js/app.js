8dd81196c5c2aa1cd9387619d07731d811b5505c
antoskins <75854261+antonypinel@users.noreply.github.com>
2022-03-15 11:34:45 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,10 @@ if ('serviceWorker' in navigator) {
     
     navigator.serviceWorker.register('/Projet_JS/assets/js/sw.js').then(
         (registration) => {
-            console.log('Service worker registration succeeded:', registration)
+            console.log('Service Worker registration succeeded:', registration)
         },
         /*catch*/ (error) => {
-            console.log('Service worker registration failed:', error)
+            console.log('Service Worker registration failed:', error)
         }
     )
 } 
