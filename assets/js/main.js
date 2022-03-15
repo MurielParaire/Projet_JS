0328076c16a201eeef9a5f31f4281e29fdc7f348
antoskins <75854261+antonypinel@users.noreply.github.com>
2022-03-15 21:18:35 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,19 @@ if ('serviceWorker' in navigator) {
     .then(() => { console.log("Service Worker Registered"); });
 }
 
+Array.from(document.querySelectorAll('nav a.title_nav')).forEach(function(element_a) {
+    element_a.addEventListener('click', 
+        function(event) {
+            console.log(event);
+            Array.from(document.querySelectorAll('.active')).forEach(function(element_section_ou_table) {
+                element_section_ou_table.removeAttribute('class');
+            });
+            let element_a_id = event.target.attributes.href.value.replace('#', '');
+            document.getElementById(element_a_id).setAttribute('class', 'active');
+        }
+    );
+});
+
 var socket = new WebSocket("wss://ws.hothothot.dog:9502");
     socket.onopen = function(event) {
         console.log("Connexion établie");
